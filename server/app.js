7b4effc1f8e4ed54cc60694d4f1853220aea4d69
Sahib <sahibxvivo@gmail.com>
2026-07-20 22:50:03 +0530

    Update CORS

--- server/app.js
+++ server/app.js
@@ -11,7 +11,15 @@ import errorMiddleware from "./middleware/errorMiddleware.js";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://devmentor-ai-three.vercel.app/",
+    ],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
